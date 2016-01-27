278b8c53cf7e07f0ec28f8f4f766d31034e9e389
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-01-27 15:36:03 -0500

    Feat: Load google maps API map of Toronto on start

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,4 +1,16 @@
 var ko = ko || {};
+var google = google || {};
+
+function initMap() {
+    'use strict';
+    console.log('initializing the map');
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 43.657899, lng: -79.3782433},
+        scrollwheel: true,
+        zoom: 13
+  });
+}
 
 var TheatreMapViewModel = function(){
     'use strict';
