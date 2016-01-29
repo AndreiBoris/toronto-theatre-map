eff302f99b3f49efdc49683560da908bae48c50f
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-01-29 10:18:11 -0500

    Feat: geocoding when no position is available

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -40,41 +40,23 @@ function wikiRequest(nameOfTheatre, viewmodel, index) {
     });
 }
 
-function coordRequest(nameOfTheatre, viewmodel, index) {
+function coordRequest(address, viewmodel, index) {
     'use strict';
 
-    // var formattedName = nameOfTheatre.replace(/ /g, '_');
+    var formattedAddress = address.replace(/ /g, '+');
 
-    // // Only try find 1 article.
-    // var urlWiki = ('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' +
-    //     formattedName + '&limit=1&redirects=resolve');
+    var urlCoords = ('https://maps.googleapis.com/maps/api/geocode/json?address=' +
+        formattedAddress + '&bounds=43.573936,-79.560076|43.758672,-79.275135' + 
+        '&key=AIzaSyA4SAawmy-oEMzdWboD0iHk9gDmmjb61o4');
 
-    // /**
-    //  * wikiRequestTimeout will be cancelled if the ajax request below is 
-    //  * successful
-    //  */
-    // var wikiRequestTimeout = setTimeout(function() { // no wiki articles found
-    //     viewmodel.infoWindows[index].setContent(mapManager.markers[index].content);
-    //     return false;
-    // }, 5000);
-
-    // $.ajax({
-    //     url: urlWiki,
-    //     dataType: 'jsonp',
-    //     success: function(data) {
-    //         // This will not let the timeout response to occur.
-    //         clearTimeout(wikiRequestTimeout);
-    //         var wikiFound = data[1].length;
-    //         if (wikiFound) {
-    //             var wikiTitle = '<h4><a href="' + data[3][0] + '">' + data[1][0] +
-    //                 '</a></h4><p>' + data[2][0] + '</p>';
-    //             viewmodel.infoWindows[index].setContent(wikiTitle);
-    //         }
-    //         if (wikiFound < 1) {
-    //             viewmodel.infoWindows[index].setContent(mapManager.markers[index].content);
-    //         }
-    //     }
-    // });
+    // TODO: perform some error handling
+    $.getJSON(urlCoords, function(data) {
+        var lat = data.results[0].geometry.location.lat;
+        var lng = data.results[0].geometry.location.lng;
+        viewmodel.markers()[index].setPosition(new google.maps.LatLng(lat, lng));
+    }).error(function(e) {
+        console.log('Failure');
+    });
 }
 
 /**
